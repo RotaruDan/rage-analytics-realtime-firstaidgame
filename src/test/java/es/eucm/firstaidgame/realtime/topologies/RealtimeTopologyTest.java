8e91b254c604c15b56ceabdd2c7522eba10301da
RotaruDan <drotaru@ucm.es>
2016-09-12 18:21:51 +0200

    Adds basic test using LocalCluster

---
+++ src/test/java/es/eucm/firstaidgame/realtime/topologies/RealtimeTopologyTest.java
@@ -0,0 +1,213 @@
+/**
+ * Copyright (C) 2016 e-UCM (http://www.e-ucm.es/)
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package es.eucm.firstaidgame.realtime.topologies;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.task.IMetricsContext;
+import es.eucm.firstaidgame.realtime.states.ESGameplayState;
+import es.eucm.firstaidgame.realtime.states.ESStateFactory;
+import es.eucm.firstaidgame.realtime.utils.Document;
+import es.eucm.firstaidgame.realtime.utils.EsConfig;
+import org.elasticsearch.client.transport.TransportClient;
+import org.junit.Test;
+import storm.trident.state.OpaqueValue;
+import storm.trident.state.State;
+import storm.trident.testing.FeederBatchSpout;
+import storm.trident.tuple.TridentTuple;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertEquals;
+
+public class RealtimeTopologyTest {
+
+    private static final Logger LOG = Logger.getLogger(RealtimeTopologyTest.class.getName());
+
+    @Test
+    public void test() throws IOException {
+        FeederBatchSpout tracesSpout = new FeederBatchSpout(Arrays.asList(
+                "versionId", "trace"));
+
+        RealtimeTopology topology = new RealtimeTopology();
+        Factory factory = new Factory(new EsConfig("testEsHost", "testSessionId"));
+        topology.prepareTest(tracesSpout, factory);
+
+        Config conf = new Config();
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("realtime", conf, topology.build());
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                ClassLoader.getSystemResourceAsStream("traces.txt")));
+
+        String line;
+        ArrayList<List<Object>> tuples = new ArrayList<List<Object>>();
+        while ((line = reader.readLine()) != null) {
+            tuples.add(Arrays.asList("version", buildTrace(line)));
+            LOG.info(line);
+
+        }
+        tracesSpout.feed(tuples);
+        for (Document document : Factory.state.documents) {
+            LOG.info(document.toString());
+        }
+
+        assertEquals(9, Factory.state.documents.size());
+
+        Player player = Factory.state.getPlayer("1");
+        assertEquals("10", player.properties.get("NotaAT"));
+        assertEquals("16", player.properties.get("NotaDT"));
+        assertEquals("13", player.properties.get("NotaINC"));
+
+        Player player2 = Factory.state.getPlayer("2");
+        assertEquals("54", player2.properties.get("NotaAT"));
+        assertEquals("65", player2.properties.get("NotaDT"));
+        assertEquals("19", player2.properties.get("NotaINC"));
+    }
+
+    private Map<String, Object> buildTrace(String line) {
+        String[] parts = line.split(",");
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("gameplayId", parts[0]);
+        map.put("timestamp", parts[1]);
+        map.put("event", parts[2]);
+        map.put("type", parts[3]);
+        map.put("target", parts[4]);
+        map.put("response", parts[5]);
+        map.put(parts[6], parts[7]);
+        return map;
+    }
+
+    public static class TestState extends ESGameplayState {
+
+        private Map<String, Player> players = new HashMap<String, Player>();
+
+        public List<Document> documents = new ArrayList<>();
+
+        public TestState(TransportClient client, String sessionId) {
+            super(client, sessionId);
+        }
+
+        @Override
+        public void setProperty(String versionId, String gameplayId,
+                                String key, Object value) {
+            getPlayer(gameplayId).setProperty(key, value);
+        }
+
+        @Override
+        public void setOpaqueValue(String versionId, String gameplayId,
+                                   List<Object> key, OpaqueValue value) {
+            getPlayer(gameplayId).setValue(keyFromList(key), value);
+        }
+
+        @Override
+        public OpaqueValue getOpaqueValue(String versionId, String gameplayId,
+                                          List<Object> key) {
+            return getPlayer(gameplayId).getValue(keyFromList(key));
+        }
+
+        private String keyFromList(List<Object> keys) {
+            String key = "";
+            for (Object o : keys) {
+                key += o;
+            }
+            return key;
+        }
+
+        @Override
+        public void bulkUpdateIndices(List<TridentTuple> inputs) {
+
+            for (TridentTuple input : inputs) {
+                Document<Map> doc = (Document<Map>) input
+                        .getValueByField("document");
+                documents.add(doc);
+
+                String key = "NotaDT";
+                Object score = doc.getSource().get(key);
+                if (score != null) {
+                    getPlayer(doc.getSource().get("gameplayId").toString()).setProperty(key, doc.getSource().get(key));
+                }
+
+                key = "NotaAT";
+                score = doc.getSource().get(key);
+                if (score != null) {
+                    getPlayer(doc.getSource().get("gameplayId").toString()).setProperty(key, doc.getSource().get(key));
+                }
+
+                key = "NotaINC";
+                score = doc.getSource().get(key);
+                if (score != null) {
+                    getPlayer(doc.getSource().get("gameplayId").toString()).setProperty(key, doc.getSource().get(key));
+                }
+            }
+        }
+
+        private Player getPlayer(String gameplayId) {
+            Player player = players.get(gameplayId);
+            if (player == null) {
+                player = new Player();
+                players.put(gameplayId, player);
+            }
+            return player;
+        }
+    }
+
+    public static class Player {
+
+        public Map<String, Object> properties = new HashMap<String, Object>();
+
+        public Map<String, OpaqueValue> values = new HashMap<String, OpaqueValue>();
+
+        public void setProperty(String key, Object value) {
+            properties.put(key, value);
+        }
+
+        public void setValue(String key, OpaqueValue value) {
+            values.put(key, value);
+        }
+
+        public OpaqueValue getValue(String key) {
+            return values.get(key);
+        }
+    }
+
+    public static class Factory extends ESStateFactory {
+
+        public static TestState state;
+
+        public Factory(EsConfig config) {
+            super(config);
+        }
+
+        @Override
+        public State makeState(Map conf, IMetricsContext metrics,
+                               int partitionIndex, int numPartitions) {
+            if (state == null) {
+                state = new TestState(null, getConfig().getSessionId());
+            }
+            return state;
+        }
+    }
+
+}
